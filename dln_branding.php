7a32d4300c0abaf0f14d1070eea2919f81d8ce09
Kristoffer Tonning <tonning.public@gmail.com>
2014-05-22 16:31:16 -0400

    added branch info

--- dln_branding.php
+++ dln_branding.php
@@ -22,6 +22,7 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path:       /languages
  * GitHub Plugin URI: tonning/dln-branding
+ * GitHub Branch: master
  */
 
 // If this file is called directly, abort.
